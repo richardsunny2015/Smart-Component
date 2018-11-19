e00066390e6fb0a6d23cc9ef61bd11f957133fb5
richardsunny2015 <richardsunny2015@gmail.com>
2018-11-18 21:26:14 -0500

    doc(index): Write comments that explain the different ways the cli app receives arguments

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const minimist = require('minimist')
+
 module.exports = () => {
-    console.log('hello')
+    const args = minimist(process.argv.slice(2)) // no flags saves all arguments in '_' property
+    console.log(args) // the very next arg after a flag is in the args object under that flag
+    // if a flag is followed by another flag, the latter flag has a property of true
 }
