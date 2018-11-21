86051256e36a5268b6357feec1a755a4f26fbf54
richardsunny2015 <richardsunny2015@gmail.com>
2018-11-21 13:29:00 -0500

    feat(parseComponentName): Write helper function

--- utilities/parseComponentName.js
+++ utilities/parseComponentName.js
@@ -1,3 +1,12 @@
-const parseComponentName = () => {}
+const parseComponentName = (fileStr) => {
+    // split the string by '/' and make alterations to the last string in the array
+    if (!fileStr) return ''
+    const splitted = fileStr.split('/')
+    const fileName = splitted[splitted.length - 1]
+    const newFileStr = fileName.slice(0, 1).toUpperCase() + fileName.slice(1)
+    if (fileName.lastIndexOf('.') < 0) return `${newFileStr}.js`
+
+    return newFileStr
+}
 
 module.exports = parseComponentName
