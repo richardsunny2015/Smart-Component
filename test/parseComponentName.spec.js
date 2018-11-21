14decfe433502b838b732ddb89e226e4c4f2951d
richardsunny2015 <richardsunny2015@gmail.com>
2018-11-21 13:16:38 -0500

    test(parseComponentName): Add first wave of tests

--- test/parseComponentName.spec.js
+++ test/parseComponentName.spec.js
@@ -1,8 +1,24 @@
-const {parseComponentName} = require('../utilities')
-const {expect} = require('chai')
+const { parseComponentName } = require('../utilities');
+const { expect } = require('chai');
 
 describe('parseComponentName', () => {
-    it('returns a string', () => {
-        expect(parseComponentName()).to.be.a('string')
-    })
-})
+  it('returns a string', () => {
+    const result = parseComponentName();
+    expect(result).to.be.a('string');
+    expect(result).to.equal('');
+  });
+  it('automatically adds .js to string if not included', () => {
+    expect(parseComponentName('Component')).to.equal('Component.js');
+  });
+  it("if specified, it won't change extension", () => {
+    expect(parseComponentName('Component.jsx')).to.equal('Component.jsx');
+    expect(parseComponentName('OneComponent.js')).to.equal('OneComponent.js');
+  });
+  it('takes a lowercase string and makes it uppercase', () => {
+    expect(parseComponentName('someComponent.js')).to.equal('SomeComponent.js');
+  });
+  it('can take directories', () => {
+      expect(parseComponentName('someDirectory/someFile')).to.equal('SomeFile.js')
+      expect(parseComponentName('someDirectory/someDirectory2/someFile')).to.equal('SomeFile.js')
+  })
+});
