d0427d4eed2e70dad9bf4a9f73d35cb10ba57aef
richardsunny2015 <richardsunny2015@gmail.com>
2018-11-21 13:36:10 -0500

    fix(test): Revise tests to be more like desired results

--- test/parseComponentName.spec.js
+++ test/parseComponentName.spec.js
@@ -7,18 +7,22 @@ describe('parseComponentName', () => {
     expect(result).to.be.a('string');
     expect(result).to.equal('');
   });
-  it('automatically adds .js to string if not included', () => {
-    expect(parseComponentName('Component')).to.equal('Component.js');
+  it('automatically removes .js or .jsx to string if included', () => {
+    expect(parseComponentName('Component.js')).to.equal('Component');
+    expect(parseComponentName('Component.jsx')).to.equal('Component');
   });
-  it("if specified, it won't change extension", () => {
-    expect(parseComponentName('Component.jsx')).to.equal('Component.jsx');
-    expect(parseComponentName('OneComponent.js')).to.equal('OneComponent.js');
+  it('if unspecified, it will return input', () => {
+    expect(parseComponentName('OneComponent')).to.equal('OneComponent');
   });
   it('takes a lowercase string and makes it uppercase', () => {
-    expect(parseComponentName('someComponent.js')).to.equal('SomeComponent.js');
+    expect(parseComponentName('someComponent.js')).to.equal('SomeComponent');
+    expect(parseComponentName('anotherComponent')).to.equal('AnotherComponent');
   });
   it('can take directories', () => {
-      expect(parseComponentName('someDirectory/someFile')).to.equal('SomeFile.js')
-      expect(parseComponentName('someDirectory/someDirectory2/someFile')).to.equal('SomeFile.js')
-  })
+    expect(parseComponentName('someDirectory/someFile')).to.equal('SomeFile');
+    expect(
+      parseComponentName('someDirectory/someDirectory2/someFile')
+    ).to.equal('SomeFile');
+    expect(parseComponentName('directoryOne/directoryTwo/directoryThree/SomeFile.jsx')).to.equal('SomeFile')
+  });
 });
